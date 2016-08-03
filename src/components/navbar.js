7a638fec9caf8946c62013dba690877fc3f1d3ed
Rajiv Ramaiah <rajivramaiah@me.com>
2016-08-02 23:27:57 -0400

    navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,33 @@
+//  Rajiv Ramaiah, CS52, Summer '16
+//  Looked through React-Youtube project from SA4 to refresh
+
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div className="NavBar">
+      <button type="Button"> New </button>
+    </div>
+  );
+};
+
+// class NavBar extends Component {
+//
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       title: '',
+//     };
+//   }
+//
+//   render() {
+//     return (
+//       <div className="NavBar">
+//         <button type="Button"> New </button>
+//       </div>
+//     );
+//   }
+// }
+
+export default NavBar;
